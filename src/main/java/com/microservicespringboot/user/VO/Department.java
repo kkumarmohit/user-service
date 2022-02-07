e689723a58e7670f5f0eab097a1da2cf994dd464
rishi aman <rishiaman@rishis-MacBook-Air.local>
2022-02-07 11:41:06 +0530

    testing rebase

--- src/main/java/com/microservicespringboot/user/VO/Department.java
+++ src/main/java/com/microservicespringboot/user/VO/Department.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Department {
+    // this is department id
     private Long departmentId;
     private String departmentName;
     private String departmentAddress;
